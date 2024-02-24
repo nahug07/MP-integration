d2be6c519b165209832a0497fae5116bd3c5bf13
ngrijalba <nahug07@gmail.com>
2024-02-23 21:44:34 -0300

    feat: integration mp done

--- client/src/components/Product/Product.tsx
+++ client/src/components/Product/Product.tsx
@@ -1,6 +1,35 @@
 import classes from './Product.module.css'
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
+import axios from 'axios';
+import { useState } from 'react';
 
 const Product = () => {
+
+    const [preferenceId, setPreferenceId] = useState(null);
+
+    initMercadoPago( import.meta.env.VITE_PUBLIC_KEY , {locale: "en-US"});
+
+    const createPreference = async () => {
+        try {
+            const response = await axios.post("http://localhost:3000/create_preference",{
+                title: "Sneakers",
+                quantity: 1,
+                price: 2000,
+            });
+
+            const {id} = response.data;
+            return id;
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const handleBuy = async () => {
+        const id = await createPreference();
+        if(id){
+            setPreferenceId(id);
+        }
+    };
 
     return (
         <div className={classes.main}>
@@ -9,11 +38,12 @@ const Product = () => {
                 <img src="https://i.pinimg.com/originals/71/81/e8/7181e84d50cb87fa4ab9a5a8ab613dbe.jpg" alt="mp-logo" />
             </div>
             <div className={classes.card}>
-                <div>
+                <div className={classes.content}>
                     <img src='https://res.cloudinary.com/dsx2jopbh/image/upload/v1708345548/cld-sample-5.jpg' alt='product img'/>
                     <h3>Sneakers</h3>
-                    <p>$ 2000</p>
-                    <button>Buy</button>
+                    <p className={classes.price}>$ 2000</p>
+                    <button className={classes.buttonPay} onClick={handleBuy}>Buy</button>
+                    {preferenceId && <Wallet initialization={{ preferenceId: preferenceId, redirectMode: 'blank' }} customization={{ texts:{ valueProp: 'smart_option'}}} />}
                 </div>
             </div>
         </div>
