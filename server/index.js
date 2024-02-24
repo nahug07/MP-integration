7a93fbeab332d27e59f323e5ec35ccf73c39840e
ngrijalba <nahug07@gmail.com>
2024-02-23 21:50:00 -0300

    style: identation code

--- server/index.js
+++ server/index.js
@@ -26,18 +26,18 @@ app.post("/create_preference", async (req, res) => {
         const body = {
             items: [
                 {
-                title: req.body.title,
-                quantity: Number(req.body.quantity),
-                unit_price: Number(req.body.price),
-                currency_id: "ARS",
-            }
+                    title: req.body.title,
+                    quantity: Number(req.body.quantity),
+                    unit_price: Number(req.body.price),
+                    currency_id: "ARS",
+                }
             ],
-        back_urls: {
-            success: "https://github.com/nahug07",
-            failure: "https://github.com/nahug07",
-            pending: "https://github.com/nahug07",
-        },
-        auto_return: "approved",
+            back_urls: {
+                success: "https://github.com/nahug07",
+                failure: "https://github.com/nahug07",
+                pending: "https://github.com/nahug07",
+            },
+            auto_return: "approved",
         };
 
         const preference = new Preference(client);
@@ -46,7 +46,7 @@ app.post("/create_preference", async (req, res) => {
             id: result.id,
         });
 
-    } catch (error){
+    } catch (error) {
         console.log(error);
         res.status(500).json({
             error: "error create reference"
