af2332d970d54f3e92b0768e08ac1f0ef21963a1
ngrijalba <nahug07@gmail.com>
2024-02-24 11:38:35 -0300

    styles: code formatting

--- client/src/components/Product/Product.tsx
+++ client/src/components/Product/Product.tsx
@@ -1,54 +1,71 @@
-import classes from './Product.module.css'
-import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
-import axios from 'axios';
-import { useState } from 'react';
+import classes from "./Product.module.css";
+import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import axios from "axios";
+import { useState } from "react";
 
 const Product = () => {
+  const [preferenceId, setPreferenceId] = useState(null);
 
-    const [preferenceId, setPreferenceId] = useState(null);
+  initMercadoPago(import.meta.env.VITE_PUBLIC_KEY, { locale: "en-US" });
 
-    initMercadoPago( import.meta.env.VITE_PUBLIC_KEY , {locale: "en-US"});
-
-    const createPreference = async () => {
-        try {
-            const response = await axios.post(import.meta.env.VITE_API + '/create_preference',{
-                title: "Sneakers",
-                quantity: 1,
-                price: 2000,
-            });
-
-            const {id} = response.data;
-            return id;
-        } catch (error) {
-            console.log(error)
+  const createPreference = async () => {
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_API + "/create_preference",
+        {
+          title: "Sneakers",
+          quantity: 1,
+          price: 2000,
         }
-    };
+      );
 
-    const handleBuy = async () => {
-        const id = await createPreference();
-        if(id){
-            setPreferenceId(id);
-        }
-    };
+      const { id } = response.data;
+      return id;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <div className={classes.main}>
-            <div className={classes.header}>
-                <h1>Integration with</h1>
-                <img src="https://i.pinimg.com/originals/71/81/e8/7181e84d50cb87fa4ab9a5a8ab613dbe.jpg" alt="mp-logo" />
-            </div>
-            <div className={classes.card}>
-                <div className={classes.content}>
-                    <img src='https://res.cloudinary.com/dsx2jopbh/image/upload/v1708345548/cld-sample-5.jpg' alt='product img'/>
-                    <h3>Sneakers</h3>
-                    <p className={classes.price}>$ 2000</p>
-                    <button className={classes.buttonPay} onClick={handleBuy}>Buy</button>
-                    {preferenceId && <Wallet initialization={{ preferenceId: preferenceId, redirectMode: 'modal' }} customization={{ texts:{ valueProp: 'smart_option'}}} />}
-                </div>
-            </div>
+  const handleBuy = async () => {
+    const id = await createPreference();
+    if (id) {
+      setPreferenceId(id);
+    }
+  };
+
+  return (
+    <div className={classes.main}>
+      <div className={classes.header}>
+        <h1>Integration with</h1>
+        <img
+          src="https://i.pinimg.com/originals/71/81/e8/7181e84d50cb87fa4ab9a5a8ab613dbe.jpg"
+          alt="mp-logo"
+        />
+      </div>
+      <div className={classes.card}>
+        <div className={classes.content}>
+          <img
+            src="https://res.cloudinary.com/dsx2jopbh/image/upload/v1708345548/cld-sample-5.jpg"
+            alt="product img"
+          />
+          <h3>Sneakers</h3>
+          <p className={classes.price}>$ 2000</p>
+          <button className={classes.buttonPay} onClick={handleBuy}>
+            Buy
+          </button>
+          {preferenceId && (
+            <Wallet
+              initialization={{
+                preferenceId: preferenceId,
+                redirectMode: "modal",
+              }}
+              customization={{ texts: { valueProp: "smart_option" } }}
+            />
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default Product
+export default Product;
